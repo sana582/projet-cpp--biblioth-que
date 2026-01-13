3afcac90a8f2549a05841d3bc1c08f67cb1c64ac
naimisalima2-cmd <naimisalima2@gmail.com>
2026-01-13 01:01:59 +0100

    Ajout classe Livre

---
+++ src/Livre.cpp
@@ -0,0 +1,66 @@
+#include "Livre.h"
+#include "Exceptions.h"
+
+// Constructeur
+Livre::Livre(const std::string& code,
+             const std::string& auteur,
+             const std::string& titre,
+             const std::string& editeur,
+             const std::string& isbn,
+             PublicCible publicCible)
+    : code_(code),
+      auteur_(auteur),
+      titre_(titre),
+      editeur_(editeur),
+      isbn_(isbn),
+      publicCible_(publicCible),
+      etat_(EtatLivre::Libre),
+      proprietaire_(nullptr),
+      emprunteurActuel_(nullptr)
+{
+    // Vérification que les champs obligatoires sont remplis
+    if (code_.empty())  throw InvalidArgumentException("code vide");
+    if (isbn_.empty())  throw InvalidArgumentException("ISBN vide");
+    if (titre_.empty()) throw InvalidArgumentException("titre vide");
+}
+
+// Getters
+const std::string& Livre::getCode() const { return code_; }
+const std::string& Livre::getAuteur() const { return auteur_; }
+const std::string& Livre::getTitre() const { return titre_; }
+const std::string& Livre::getEditeur() const { return editeur_; }
+const std::string& Livre::getISBN() const { return isbn_; }
+PublicCible Livre::getPublicCible() const { return publicCible_; }
+EtatLivre Livre::getEtat() const { return etat_; }
+
+// Indique si le livre est disponible
+bool Livre::estLibre() const {
+    return etat_ == EtatLivre::Libre;
+}
+
+// Retourne la bibliothèque propriétaire du livre
+Bibliotheque* Livre::getProprietaire() const { return proprietaire_; }
+
+// Retourne l'adhérent qui a emprunté le livre
+Adherent* Livre::getEmprunteurActuel() const { return emprunteurActuel_; }
+
+// Définit la bibliothèque propriétaire du livre
+void Livre::setProprietaire(Bibliotheque* b) {
+    proprietaire_ = b;
+}
+
+// Définit l'adhérent qui emprunte le livre
+void Livre::setEmprunteur(Adherent* a) {
+    emprunteurActuel_ = a;
+}
+
+// Change l'état du livre
+void Livre::setEtat(EtatLivre e) {
+    etat_ = e;
+}
+
+// Surcharge de l'opérateur << pour afficher un livre
+std::ostream& operator<<(std::ostream& os, const Livre& l) {
+    l.afficher(os);
+    return os;
+}
---
+++ src/Livre.h
@@ -0,0 +1,65 @@
+#ifndef LIVRE_H
+#define LIVRE_H
+
+#include <string>
+#include <ostream>
+#include "Enums.h"
+
+class Bibliotheque;
+class Adherent;
+
+// Classe abstraite représentant un livre
+class Livre {
+protected:
+    // Informations du livre
+    std::string code_;
+    std::string auteur_;
+    std::string titre_;
+    std::string editeur_;
+    std::string isbn_;
+    PublicCible publicCible_;
+    EtatLivre etat_;
+
+    Bibliotheque* proprietaire_;
+    Adherent* emprunteurActuel_;
+
+public:
+    // Constructeur
+    Livre(const std::string& code,
+          const std::string& auteur,
+          const std::string& titre,
+          const std::string& editeur,
+          const std::string& isbn,
+          PublicCible publicCible);
+
+    virtual ~Livre() = default;
+
+    // Getters
+    const std::string& getCode() const;
+    const std::string& getAuteur() const;
+    const std::string& getTitre() const;
+    const std::string& getEditeur() const;
+    const std::string& getISBN() const;
+    PublicCible getPublicCible() const;
+    EtatLivre getEtat() const;
+
+    // Retourne le type du livre
+    virtual TypeLivre getType() const = 0;
+
+    // Indique si le livre est libre
+    bool estLibre() const;
+
+    Bibliotheque* getProprietaire() const;
+    Adherent* getEmprunteurActuel() const;
+
+    void setProprietaire(Bibliotheque* b);
+    void setEmprunteur(Adherent* a);
+    void setEtat(EtatLivre e);
+   
+    // Affichage du livre
+    virtual void afficher(std::ostream& os) const = 0;
+
+    friend std::ostream& operator<<(std::ostream& os, const Livre& l);
+};
+
+#endif
